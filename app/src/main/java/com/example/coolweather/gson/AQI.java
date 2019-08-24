6e487ec3a02c00a8cb68973587c423da2e3ee900
pengchenyu <1106998960@qq.com>
2019-08-24 15:05:44 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,16 @@
+package com.example.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity{
+        @SerializedName("aqi")
+        public String aqi;
+
+        @SerializedName("pm25")
+        public String pm25;
+    }
+}
